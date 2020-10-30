bc4e8e8a92a771bcfd6cf2393e2b1b5843803850
changjiang <zhaochangjiang@modian.com>
2020-10-30 11:19:02 +0800

    修改版本选取

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/juetun/project
 go 1.15
 
 require (
+	github.com/go-vgo/robotgo v0.91.0
 	github.com/juetun/base-wrapper v0.0.30
+	github.com/manifoldco/promptui v0.8.0
 	github.com/spf13/viper v1.7.1
 )
--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 	"github.com/juetun/base-wrapper/lib/app_obj"
 	"github.com/juetun/base-wrapper/lib/common"
-	"github.com/juetun/project/utils/bar"
+	"github.com/manifoldco/promptui"
 	"os/exec"
 	"strconv"
 	"strings"
-	"sync"
-	"time"
 )
 
 type FlagParams struct {
@@ -21,45 +19,27 @@ func main() {
 	// 定义几个变量，用于接收命令行的参数
 	NewFlagParameter().InitFlag().
 		ParamsImplement()
-	s := fmt.Sprintf("\033[s")
-	fmt.Printf("kkkk:%s \n", s)
-	fmt.Println("1dsafasd")
-	fmt.Println("2dsafasd")
-	fmt.Println("3dsafasd")
-	fmt.Println("4dsafasd")
-	fmt.Println("5dsafasd")
-	fmt.Println("6dsafasd")
-	fmt.Println("7dsafasd")
-	fmt.Println("8dsafasd")
-	fmt.Println("9dsafasd")
-	fmt.Println("10dsafasd")
 
-	fmt.Printf("\033[s")
-	var vString string
-	fmt.Printf("\033[%dA", 4)
-	fmt.Scanln(&vString) //读取键盘的输入，通过操作地址，赋值给x和y   阻塞式
-	//fmt.Printf("\033[u")
-	fmt.Printf("\033[%dB", 4)
-	fmt.Printf("您输入的内容为:%s \n", vString)
-	//Bar()
-}
-func Bar() {
-
-	pgbar := bar.New("多线程进度条")
-	bar.Println("进度条1")
-	b := pgbar.NewBar("1st", 20000)
-	b.SetSpeedSection(900, 100)
-	//bar.Println("独立进度条")
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 20000; i++ {
-			b.Add()
-			time.Sleep(time.Second / 2000)
-		}
-	}()
-	wg.Wait()
+	//s := fmt.Sprintf("\033[s")
+	//fmt.Printf("kkkk:%s \n", s)
+	//fmt.Println("1dsafasd")
+	//fmt.Println("2dsafasd")
+	//fmt.Println("3dsafasd")
+	//fmt.Println("4dsafasd")
+	//fmt.Println("5dsafasd")
+	//fmt.Println("6dsafasd")
+	//fmt.Println("7dsafasd")
+	//fmt.Println("8dsafasd")
+	//fmt.Println("9dsafasd")
+	//fmt.Println("10dsafasd")
+	//
+	//fmt.Printf("\033[s")
+	//var vString string
+	//fmt.Printf("\033[%dA", 4)
+	//fmt.Scanln(&vString) //读取键盘的输入，通过操作地址，赋值给x和y   阻塞式
+	////fmt.Printf("\033[u")
+	//fmt.Printf("\033[%dB", 4)
+	//fmt.Printf("您输入的内容为:%s \n", vString)
 }
 
 type FlagParameter struct {
@@ -72,53 +52,76 @@ func NewFlagParameter() (res *FlagParameter) {
 
 //各个参数命令的实现
 func (r *FlagParameter) ParamsImplement() {
-	var err error
 	switch r.flagParameter.Release {
 	case "develop":
-		version := r.develop()
-		err= exec.Command("git","checkout master").Run()
-		if err != nil {
-			panic(err.Error())
-		}
+		r.develop()
 
-		err = exec.Command("git", "branch", fmt.Sprintf("release/%s", version)).Run()
-		if err != nil {
-			panic(err.Error())
-		}
 	}
 }
 
-func (r *FlagParameter) develop() string {
-	version := r.getVersion()
-	versionList := r.orgVersion(version)
-	fmt.Printf("您可以选择如下版本的：\n")
-	defaultValue := ""
-	var v string
-	for _, value := range versionList {
+func (r *FlagParameter) develop() {
 
-		if defaultValue == "" {
-			defaultValue = value
-			v = fmt.Sprintf("v%s", value)
-			v = fmt.Sprintf("》%s", v)
-		} else {
-			v = fmt.Sprintf("v%s", value)
+	version := r.selectVersion()
+
+	//执行当前版本要做的事
+	r.cmdDevelopByVersion(version)
+
+}
+
+type CmdObject struct {
+	Name string   `json:"name"`
+	Arg  []string `json:"arg"`
+}
+
+func (r *FlagParameter) cmdDevelopByVersion(version string) {
+
+	cmdSlice := []CmdObject{
+		//{Name: "git", Arg: []string{"checkout", "master",},},
+		//{Name: "git", Arg: []string{"branch", fmt.Sprintf("release/%s", version),}},
+		{Name: "git", Arg: []string{"checkout", "-b", fmt.Sprintf("release/%s", version), "master"}},
+		//{Name: "sed", Arg: []string{"-s", `version: "1.0.0"`, fmt.Sprintf(`version: "%s"`,version)}},
+		{Name: "sed", Arg: []string{"-n", "2p", "./config/app.yaml",}},
+	}
+	fmt.Printf("创建分支")
+	for _, item := range cmdSlice {
+		err := r.exeCMD(&item)
+		if err != nil {
 		}
-		fmt.Printf("%s\n", v)
 	}
 
-	var vString string
-	fmt.Println("请选择版本：")
-	fmt.Scanln(&vString) //读取键盘的输入，通过操作地址，赋值给x和y   阻塞式
-	if vString == "" {
-		vString = defaultValue
+}
+func (r *FlagParameter) exeCMD(item *CmdObject) (err error) {
+	var buf []byte
+	cmdString := []interface{}{"【CMD】:", item.Name}
+	for _, value := range item.Arg {
+		cmdString = append(cmdString, value)
+	}
+	fmt.Println(cmdString...)
+	cmd := exec.Command(item.Name, item.Arg...)
+	buf, _ = cmd.Output()
+	fmt.Printf("%s\n", buf)
+	return
+}
+
+//版本选择交互
+func (r *FlagParameter) selectVersion() (res string) {
+	v := r.getVersion()
+	versionList := r.orgVersion(v)
+	fmt.Printf("Now version is :%s \r\n",v)
+	prompt := promptui.Select{
+		Label: fmt.Sprintf("Select Version",),
+		Items: versionList,
+	}
+	_, vString, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
 	}
 	fmt.Printf("您选择的版本为：%s \n", vString)
 	return vString
-	//var reader = bufio.NewReader(os.Stdin) //os.Stdin标椎输入（键盘）
-	//s1, _ := reader.ReadString('\n')       //读到\n结束
-	//fmt.Println("读到的数据：", s1)
 }
 
+//获取当前可选择的版本
 func (r *FlagParameter) orgVersion(version string) (res []string) {
 	var err error
 	var v int
