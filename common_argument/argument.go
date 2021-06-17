f7785f96d8a74930a495d12e56dff65eddd69f6b
changjiang <zhaochangjiang@modian.com>
2021-06-17 12:07:25 +0800

    升级依赖包

--- common_argument/argument.go
+++ common_argument/argument.go
@@ -1,14 +1,17 @@
 package common_argument
 
 import (
-	"github.com/juetun/base-wrapper/lib/app_obj"
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
+	"github.com/juetun/base-wrapper/lib/app/app_start"
 	"github.com/juetun/base-wrapper/lib/common"
+	"github.com/juetun/base-wrapper/lib/plugins"
 )
 
 var CommonArgument = CommonArgumentStruct{}
 
 func init() {
-	common.PluginsApp()
+	var preLoad=&app_start.PluginsOperate{}
+	plugins.PluginsApp(preLoad)
 	CommonArgument.Application = common.GetAppConfig()
 }
 
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/juetun/project
 go 1.15
 
 require (
-	github.com/go-vgo/robotgo v0.91.0
-	github.com/juetun/base-wrapper v0.0.30
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/juetun/base-wrapper v0.0.70
 	github.com/manifoldco/promptui v0.8.0
-	github.com/prometheus/common v0.4.0
-	github.com/spf13/viper v1.7.1
+	github.com/prometheus/common v0.10.0
 )
